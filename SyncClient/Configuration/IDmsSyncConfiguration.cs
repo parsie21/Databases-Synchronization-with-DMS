a45bd627db5d097e71ee590b742ea869d898ffb0
parsie21 <lucaparise100@gmail.com>
2025-08-20 14:43:49 +0200

    I  don't know what i'm doing. Might have broken something

---
+++ SyncClient/Configuration/IDmsSyncConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncClient.Console.Configuration
+{
+    public interface IDmsSyncConfiguration
+    {
+        string GetConnectionString(string name);
+        string GetValue(string key);
+
+        /*
+        POSSIBILI METODI FUTURI DI VALIDAZIONE 
+        ----------------------------
+        bool isConnectionStringValid
+        bool isValuePresent
+        bool tryGetValue
+        bool validateAll
+        */
+    }
+}
